c47d09be579a6d4cfeb83397a479912adcff855e
aniketpandit22 <163455317+aniketpandit22@users.noreply.github.com>
2024-07-05 19:53:27 +0530

    Add files via upload

---
+++ ATM-Machine-main/ATM-Machine-main/main.java
@@ -0,0 +1,15 @@
+
+package ATMmachine;
+
+   import java.sql.SQLException; 
+
+public class main {
+
+   
+    public static void main(String[] args)throws InterruptedException,
+            SQLException{
+        Login login = new Login();   
+        login.loginView(); 
+    }
+    
+}
